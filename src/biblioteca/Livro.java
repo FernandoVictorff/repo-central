628ad875fb57ee9e8b7b6f289e7aadbebc1ea0e6
Fernando Victor <fernandovictor01@gmail.com>
2021-05-03 16:56:19 -0300

    Dados de livro e usuário Classe bibliotecario

--- src/biblioteca/Livro.java
+++ src/biblioteca/Livro.java
@@ -1,14 +1,14 @@
 package biblioteca;
 
 public class Livro {
-	
-	private String titulo; 
+	private String titulo;
+	private String numeroDepagina;
+	private String autor;
 	
 	public void emprestar() {
 		
 	}
 	
 	public void devolver() {
-		
 	}
 }
